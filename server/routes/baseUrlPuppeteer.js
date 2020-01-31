264a08076d8f246308b78f7217128d11cb00f39a
Bas Suckling <suckling.w.s@gmail.com>
2020-01-31 15:30:07 +1300

    testing puppeteer

--- server/routes/baseUrlPuppeteer.js
+++ server/routes/baseUrlPuppeteer.js
@@ -1,11 +1,17 @@
+// test file to see if I can get puppeteer to render a web page before I get the links.
+
 const puppeteer = require('puppeteer');
 
-(async () => {
+function getBaseUrl (url) {(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('http://www.bas-suckling.com');
+  await page.goto(url);
   let content = await(page.content());
   await browser.close();
   console.log(content)
 })();
+}
 
+module.exports = {
+    getBaseUrl
+}
