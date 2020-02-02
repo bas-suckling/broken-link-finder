3f1c70ea145259bc39fe1bfd9c2630c9fda3fca6
Bas Suckling <suckling.w.s@gmail.com>
2020-02-03 10:47:13 +1300

    ongoing implementation attempt of puppeteer

--- client/helpers.js
+++ client/helpers.js
@@ -1,7 +1,24 @@
+const puppeteer = require('puppeteer')
+
+const scrapeUrl = async (url) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+
+  const scrapedData = await(page.content());
+  await browser.close();
+
+  await browser.close()
+  return scrapedData
+}
+
+
+
 function findEmbeddedLinks(wallOfText) {
     return arrayOfUrls
 }
 
 module.exports = {
-    findEmbeddedLinks
+    findEmbeddedLinks,
+    scrapeUrl
 }
--- server/routes/baseUrlScraper.js
+++ server/routes/baseUrlScraper.js
@@ -1,34 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const puppeteer = require('puppeteer');
-
-const request = require ('superagent')
-
-// router.get('/:url', (req,res) => {
-//     let url = req.params.url
-//     console.log("url is", url )
-//     request
-//     .get(url)
-//     .then(x => res.send(x))
-//     .catch(err => res.send(err.status))
-    
-// })
-
+const {scrapeUrl} = '../../client/helpers.js'
+// const request = require ('superagent')
 
 router.get('/:url', (req, res) => {
-    let url = req.params.url
-    getBaseUrl(url)
-    .then(content => console.log(content))
-} )
+  let url = toString(req.params.url)
+  console.log('url is', url)
+  const scrapedPage = new Promise((resolve, reject) => {
+    scrapeUrl(url)
+      .then(data => {
+        resolve(data)
+      })
+      .catch(err => reject('Page scrape failed', err))
+  })
 
-function getBaseUrl (url) {(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    let content = await(page.content());
-    await browser.close();
-    return content
-  })();
-  }
+
+  Promise.all(scrapedPage)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => res.status(500).send(err))
+})
+
+
 module.exports = router
 
