ee3a3240a76d85711259f0075ad8e0ba5d77f821
Bas Suckling <suckling.w.s@gmail.com>
2020-02-03 14:28:30 +1300

    ongoing development with puppeteer

--- client/helpers.js
+++ client/helpers.js
@@ -1,18 +1,3 @@
-const puppeteer = require('puppeteer')
-
-const scrapeUrl = async (url) => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(url)
-
-  const scrapedData = await(page.content());
-  await browser.close();
-
-  await browser.close()
-  return scrapedData
-}
-
-
 
 function findEmbeddedLinks(wallOfText) {
     return arrayOfUrls
@@ -20,5 +5,4 @@ function findEmbeddedLinks(wallOfText) {
 
 module.exports = {
     findEmbeddedLinks,
-    scrapeUrl
 }
