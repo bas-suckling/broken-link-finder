4e68d18978f36513f55746bd44551602d8b990d9
robert-cummins <robert.k.cummins@gmail.com>
2020-02-04 14:04:03 +1300

    storing scrapped data in backend

--- client/api/index.js
+++ client/api/index.js
@@ -1,9 +1,10 @@
 import request from 'superagent'
 
 export default function scrapeBaseUrl(url) {
+    console.log('hello')
     return request
         .get('/api/v1/scraper/'+url)
-        .then(res => (res))
+        .then(res => console.log(res.body))
 }
 
 
--- server/routes/baseUrlScraper.js
+++ server/routes/baseUrlScraper.js
@@ -1,36 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const puppeteer = require('puppeteer');
 // const scrapeUrl = require('../../client/helpers.js')
 // const getBaseUrl = require('./baseUrlPuppeteer')
 
 router.get('/:url', (req, res) => {
-  let url = req.params.url
-  console.log('url is', url)
-  const scrapedPage = new Promise((resolve, reject) => {
-    getBaseUrlContent(url)
-      .then(data => {
-        resolve(data)
-      })
-      .catch(err => reject('Medium scrape failed'))
-  })
-    
-  Promise.all(scrapedPage)
-    .then(data => {
-      console.log(data)
-    })
-    .catch(err => res.status(500).send(err))
-})
-
-module.exports = router
-
-const puppeteer = require('puppeteer');
-
-function getBaseUrlContent (url) {(async () => {
+  let data
+  let url2 = "https://" + req.params.url
+  function getBaseUrlContent (url) {(async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  let content = await(page.content());
+  let content = await(page.content())
   await browser.close();
-  return(content)
+  data = content
+  // seperateLinks(data)
 })();
 }
+
+getBaseUrlContent(url2)
+// setTimeout(function(){ console.log(data); }, 5000);
+ 
+})
+
+
+module.exports = router
+
+
+
