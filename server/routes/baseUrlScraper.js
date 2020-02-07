2f28718359017abb550b5a18ef3df745d1b0fe78
Bas Suckling <suckling.w.s@gmail.com>
2020-02-07 14:30:19 +1300

    broken refactor

--- server/routes/baseUrlScraper.js
+++ server/routes/baseUrlScraper.js
@@ -6,46 +6,37 @@ const arrayFunctions = require('./linkTest')
 
 
 router.get('/:url', (req, res) => {
-  let data
-  let url2 = "https://" + req.params.url
-  function getBaseUrlContent (url) {(async () => {
+  let url = "https://" + req.params.url
+
+  let response = await Promise.all(getBaseUrlContent(url))
+
+  res.json(response)
+
+})
+
+function  getBaseUrlContent(url) {
+  
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
     const elementHandles = await page.$$('a');
-      const propertyJsHandles = await Promise.all(
-        elementHandles.map(handle => handle.getProperty('href'))
-      );
-    
-      const hrefs2 = await Promise.all(
-        propertyJsHandles.map(handle => handle.jsonValue())
-      );
+    const propertyJsHandles = await Promise.all(
+      elementHandles.map(handle => handle.getProperty('href'))
+    );
+    const arrayOfLinks = await Promise.all(
+      propertyJsHandles.map(handle => handle.jsonValue())
+    );
     await browser.close();
-    data = hrefs2
-   let stats = await Promise.all(arrayFunctions.testAllLinks(data))
-   .then(res => {
-    return res
-   })
-   let finalObj = arrayFunctions.generateObjArray(data, stats)
-   console.log(finalObj)
-  //  res.send(stats)
-   res.json(finalObj)
-    
-    
-  })();
 
+    let array = await Promise.all(arrayFunctions.testAllLinks(arrayOfLinks))
+      .then(res => {
+        return res
+      })
 
+    let finalObj = arrayFunctions.generateObjArray(arrayOfLinks, array)
+    console.log(finalObj)
+    return finalObj
 
 }
 
-getBaseUrlContent(url2)
-// setTimeout(function(){ console.log(data); }, 5000);
- 
-})
-
-
-
 module.exports = router
-
-
-
