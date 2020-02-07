cf4f7414047903048f8aa3574869110f157ea881
Bas Suckling <suckling.w.s@gmail.com>
2020-02-07 15:00:03 +1300

    working refactor

--- server/routes/baseUrlScraper.js
+++ server/routes/baseUrlScraper.js
@@ -8,35 +8,40 @@ const arrayFunctions = require('./linkTest')
 router.get('/:url', (req, res) => {
   let url = "https://" + req.params.url
 
-  let response = await Promise.all(getBaseUrlContent(url))
-
-  res.json(response)
-
+  getBaseUrlContent(url).then(response => {
+    res.json(response)
+  })
 })
 
-function  getBaseUrlContent(url) {
-  
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const elementHandles = await page.$$('a');
-    const propertyJsHandles = await Promise.all(
-      elementHandles.map(handle => handle.getProperty('href'))
-    );
-    const arrayOfLinks = await Promise.all(
-      propertyJsHandles.map(handle => handle.jsonValue())
-    );
-    await browser.close();
-
-    let array = await Promise.all(arrayFunctions.testAllLinks(arrayOfLinks))
-      .then(res => {
-        return res
+function getBaseUrlContent(url) {
+    return puppeteer.launch()
+    .then(browser => {
+      return browser.newPage()
+      .then(page => {
+        return page.goto(url)
+        .then(() => {
+          return page.$$('a')
+          .then(elementHandles => {
+            return Promise.all(elementHandles.map(handle => handle.getProperty('href')))
+            .then(propertyJsHandles => {
+              return Promise.all(propertyJsHandles.map(handle => handle.jsonValue()))
+              .then(arrayOfLinks => {
+                return browser.close()
+                .then(() => {
+                  return Promise.all(arrayFunctions.testAllLinks(arrayOfLinks))
+                  .then(array => {
+                    console.log(array)
+                    console.log('array')
+                    return arrayFunctions.generateObjArray(arrayOfLinks, array)
+                  })
+                })
+              })
+              })
+            })
+          }) 
       })
-
-    let finalObj = arrayFunctions.generateObjArray(arrayOfLinks, array)
-    console.log(finalObj)
-    return finalObj
-
+  })
 }
+    
 
 module.exports = router
