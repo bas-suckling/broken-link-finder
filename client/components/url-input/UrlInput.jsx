d39ad193613654aaa1fef728af825503486f647d
Bas Suckling <suckling.w.s@gmail.com>
2020-02-12 11:27:12 +1300

    update placeholder link

--- client/components/url-input/UrlInput.jsx
+++ client/components/url-input/UrlInput.jsx
@@ -34,7 +34,7 @@ class UrlInput extends React.Component {
                 <div>
                     <h3>Enter a url into the box below to find all broken links on that page</h3>
                     <form>
-                        <input className='' required type='text' name='url' placeholder='eg. www.askjeeves.com' onChange={this.updateUrl}></input>
+                        <input className='' required type='text' name='url' placeholder='eg. www.google.com' onChange={this.updateUrl}></input>
                         <button className="" onClick={this.submit}>
                             Check links
                         </button>
