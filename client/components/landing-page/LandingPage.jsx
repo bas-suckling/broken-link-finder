b923dd4e75cdb52d441872b3bc65d6e75e252fb6
Bas Suckling <suckling.w.s@gmail.com>
2020-05-04 14:10:39 +1200

    update link to linkedin

--- client/components/landing-page/LandingPage.jsx
+++ client/components/landing-page/LandingPage.jsx
@@ -10,7 +10,7 @@ class LandingPage extends React.Component {
         <>
           <div className='content'>
             <h1 className="title">Broken Link Finder</h1>
-            <h5 className="animated fadeIn">Made by <a href="www.linkedin.com/in/sebastian-suckling-33ba7899">Bas Suckling</a> </h5>
+            <h5 className="animated fadeIn">Made by <a href="https://www.linkedin.com/in/sebastian-suckling-33ba7899">Bas Suckling</a> </h5>
 
             <UrlInput/>
           </div>
